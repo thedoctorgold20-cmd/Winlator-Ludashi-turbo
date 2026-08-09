1478a91e389520f015ffda3a05131379908237fc
thedoctorgold20-cmd <thedoctorgold20@gmail.com>
2026-08-09 09:38:52 -0300

    Update memory.h

--- app/src/main/cpp/memory.h
+++ app/src/main/cpp/memory.h
@@ -1,23 +1,16 @@
 #ifndef WINLATOR_MEMORY_H
 #define WINLATOR_MEMORY_H
 
-#include <cstddef>
 #include <cstdint>
 
 namespace WinlatorMemory {
 
-constexpr uint64_t VIRTUAL_RAM_16GB = 16ULL * 1024ULL * 1024ULL * 1024ULL;
+constexpr uint64_t VIRTUAL_RAM_12GB =
+        12ULL * 1024ULL * 1024ULL * 1024ULL;
 
-uint64_t getPhysicalRAM();
-uint64_t getConfiguredRAM();
-
-bool reserveMemory(std::size_t size);
-void releaseMemory(std::size_t size);
-
-std::size_t getReservedMemory();
-
-void initialize();
-void shutdown();
+uint64_t getVirtualTotalMemory();
+uint64_t getVirtualAvailableMemory();
+uint64_t getVirtualUsedMemory();
 
 }
 
